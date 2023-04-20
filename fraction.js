60209e14e2ac1b4282ecff54833d5b564eb4cc0f
denispaul2406 <117253338+denispaul2406@users.noreply.github.com>
2023-04-20 11:27:54 +0530

    Update fraction.js

--- fraction.js
+++ fraction.js
@@ -1,1 +1,16 @@
+class Fraction {
+  constructor(numerator, denominator) {
+    this.numerator = numerator;
+    this.denominator = denominator;
+  }
+add(otherFraction) {
+    let commonDenominator = this.denominator * otherFraction.denominator;
+    let numeratorSum = (this.numerator * otherFraction.denominator) + (otherFraction.numerator * this.denominator);
+    return new Fraction(numeratorSum, commonDenominator);
+  }
+
+  subtract(otherFraction) {
+    let commonDenominator = this.denominator * otherFraction.denominator;
+    let numeratorDiff = (this.numerator * otherFraction.denominator) - (otherFraction.numerator * this.denominator);
+    return new Fraction(numeratorDiff, commonDenominator);
 
