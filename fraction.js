e15dca79336376233560a3f81d19dbd8ef7dc878
MeghanaH23 <112924802+MeghanaH23@users.noreply.github.com>
2023-04-20 11:32:09 +0530

    Update fraction.js

--- fraction.js
+++ fraction.js
@@ -1,1 +1,10 @@
-
+compare(otherFraction) {
+    let thisDecimal = this.numerator / this.denominator;
+    let otherDecimal = otherFraction.numerator / otherFraction.denominator;
+    if (thisDecimal < otherDecimal) {
+      return -1;
+    } else if (thisDecimal > otherDecimal) {
+      return 1;
+    } else {
+      return 0;
+    }
