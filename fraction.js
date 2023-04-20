51c77ca4a0046ff9eff0559b3555afd1da9dd23c
likith47 <117799918+likith47@users.noreply.github.com>
2023-04-20 11:29:01 +0530

    Update fraction.js

--- fraction.js
+++ fraction.js
@@ -1,1 +1,6 @@
+ divide(otherFraction) {
+    let quotientNumerator = this.numerator * otherFraction.denominator;
+    let quotientDenominator = this.denominator * otherFraction.numerator;
+    return new Fraction(quotientNumerator, quotientDenominator);
+  }
 
